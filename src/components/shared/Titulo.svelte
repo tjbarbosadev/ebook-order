b0b7f9c474f170c600d02dcd91afdfb2502cc723
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-06-12 14:32:10 -0300

    feat: titulo component

---
+++ src/components/shared/Titulo.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { children, titulo = 'Título' } = $props();
+</script>
+
+<div class="flex flex-col">
+	<h1 class="text-3xl font-black">{titulo}</h1>
+	<p class="text-sm text-zinc-400">
+		{@render children?.()}
+	</p>
+</div>
--- src/routes/autores/+page.svelte
+++ src/routes/autores/+page.svelte
@@ -1,11 +1,18 @@
 <script>
 	import ItemAutor from '../../components/autor/ItemAutor.svelte';
+	import Titulo from '../../components/shared/Titulo.svelte';
 	import autores from '../../constants/autores';
 </script>
 
-<h1 class="text-3xl font-bold">Autores</h1>
-<div class="grid grid-cols-2 gap-4">
-	{#each autores as autor}
-		<ItemAutor {autor} />
-	{/each}
+<div class="flex flex-col gap-5">
+	<Titulo titulo="Autores">
+		Conheça os autores por trás das maiores obras de fantasia, suas histórias de vida, inspirações e
+		contribuições para o mundo da literatura.
+	</Titulo>
+
+	<div class="grid grid-cols-2 gap-4">
+		{#each autores as autor}
+			<ItemAutor {autor} />
+		{/each}
+	</div>
 </div>
